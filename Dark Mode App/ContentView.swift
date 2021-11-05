6e53019b2b568ef34c9134f405d20d1072b64bae
mariusaee <mariusae@me.com>
2021-11-05 14:27:27 +0300

    Initial Commit

---
+++ Dark Mode App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dark Mode App
+//
+//  Created by Marius Malyshev on 05.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dark Mode App/Dark_Mode_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dark_Mode_AppApp.swift
+//  Dark Mode App
+//
+//  Created by Marius Malyshev on 05.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Dark_Mode_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
